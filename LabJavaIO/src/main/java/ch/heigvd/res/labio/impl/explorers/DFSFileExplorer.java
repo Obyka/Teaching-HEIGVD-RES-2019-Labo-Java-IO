c1b14a5f74d0d8a080fc9aee24ab2a30b9a81346
Florian <florian.polier@heig-vd.ch>
2019-03-10 21:26:46 +0100

    DFS Implemented. Not sure about the order of actions though.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +17,24 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      // Si rootDirectory est un fichier, on a fini le parcours
+      vistor.visit(rootDirectory);
+      if(rootDirectory.isFile()){
+          return;
+      }
+      // On liste les éléments présents dans le répertoire courant et on trie le résultat
+      File[] currentFile = rootDirectory.listFiles();
+      if(currentFile == null){
+        return;
+      }
+      Arrays.sort(currentFile);
+
+      // Pour chaque élément dans le répertoire, on appelle le visiteur et on continue le parcours dans les sous-dossier
+      for(File f : currentFile){
+        if(f.isDirectory()){
+            explore(f, vistor);
+        }
+    }
   }
 
 }
